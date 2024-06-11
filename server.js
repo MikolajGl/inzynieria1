1594491ca1a1ddeb05843ed36d43ac6f5f29c2a1
ELOKT <ul0266104@edu.uni.lodz.pl>
2024-06-11 22:07:41 +0200

    1.3

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.json()); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 let users = [];
 let messages = [];
+
+const PORT = process.env.PORT || 3000;
 
 app.post('/login', (req, res) => {
     const { username } = req.body;
@@ -40,6 +41,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
