9457e39e738dc2bf68d2fa4f7ac472045e154e68
ELOKT <ul0266104@edu.uni.lodz.pl>
2024-06-11 18:36:50 +0200

    1.1

---
+++ server.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const express = require ("express");
+const app = express();
+const PORT = 3000 || process.env.PORT;
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT,() => console.log(`server running on port ${PORT}`));
